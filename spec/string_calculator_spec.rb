be90a92e63be1267f13ee0ea848d9da3e4eadfac
Abhijith-Syam <abhijithsyam456@gamil.com>
2025-08-30 12:33:27 +0530

    Add test for multi character custom delimiter, pass - GREEN

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -61,5 +61,11 @@ RSpec.describe StringCalculator do
 
       expect(calculator.add("2,1001")).to eq(2)
     end
+
+    it 'returns the sum if custom delimiter with multiple characters between numbers' do
+      calculator = StringCalculator.new
+
+      expect(calculator.add("//[***]\n1***2***3")).to eq(6)
+    end
   end
 end
