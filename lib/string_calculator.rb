eb8a2bbc5603588ecbb2d79a3eccc57f9505d3ce
Abhijith-Syam <abhijithsyam456@gamil.com>
2025-08-30 11:53:30 +0530

    Make test for multiple numbers with custom delimiter between, pass - GREEN

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -9,6 +9,15 @@ class StringCalculator
 
   # Returns array of integers from a string of comma-separated numbers
   def extract_operands_from_string(argument_string)
-    argument_string.split(/[\n,]/).map(&:to_i)
+    number_string = argument_string
+    delimiter = /[\n,]/
+    if argument_string.start_with?("//")
+      lines = argument_string.split("\n", 2)
+      custom_delimiter = lines[0][2..-1]
+      delimiter = /[\n,#{Regexp.escape(custom_delimiter)}]/
+      number_string = lines[1]
+    end
+
+    number_string.split(delimiter).map(&:to_i)
   end
 end
