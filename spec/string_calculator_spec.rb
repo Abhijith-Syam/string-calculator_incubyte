1bc7e657582ef1cd3f309cb59efe726acb33abb4
Abhijith-Syam <abhijithsyam456@gamil.com>
2025-08-30 10:45:22 +0530

    Initial project setup with RSpec

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe StringCalculator do
+
+  describe '#add' do
+
+    it 'returns 0 for an empty string' do
+      calculator = StringCalculator.new
+
+      expect(calculator.add("")).to eq(0)
+    end
+  end
+end
