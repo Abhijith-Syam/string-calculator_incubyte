33da636b1bdef2268517f8e1f67f8639ef1aee84
Abhijith-Syam <abhijithsyam456@gamil.com>
2025-08-30 12:04:45 +0530

    Refactor: To use a separate method for extracting the delimiter and number string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,13 +2,19 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    extract_operands_from_string(numbers).sum
+    delimiter, number_string = extract_delimiter_and_number_string(numbers)
+    extract_operands_from_string(number_string, delimiter).sum
   end
 
   private
 
   # Returns array of integers from a string of comma-separated numbers
-  def extract_operands_from_string(argument_string)
+  def extract_operands_from_string(number_string, delimiter)
+    number_string.split(delimiter).map(&:to_i)
+  end
+
+  # Returns the default or custom delimeter if any and the string containing the operand numbers
+  def extract_delimiter_and_number_string(argument_string)
     number_string = argument_string
     delimiter = /[\n,]/
     if argument_string.start_with?("//")
@@ -18,6 +24,6 @@ class StringCalculator
       number_string = lines[1]
     end
 
-    number_string.split(delimiter).map(&:to_i)
+    [delimiter, number_string]
   end
 end
