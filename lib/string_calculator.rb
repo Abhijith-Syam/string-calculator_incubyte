96ace29cacc652a58617ae920d88c8733b9794cf
Abhijith-Syam <abhijithsyam456@gamil.com>
2025-08-30 11:14:51 +0530

    Refactor: To use a private method for extracting the operands from the argument string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,13 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(',').map(&:to_i).sum.to_i
+    extract_operands_from_string(numbers).sum
+  end
+
+  private
+
+  # Returns array of integers from a string of comma-separated numbers
+  def extract_operands_from_string(argument_string)
+    argument_string.split(',').map(&:to_i)
   end
 end
